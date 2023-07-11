676f93286fe952771daee67ac53485ae55cc1973
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2023-07-12 01:46:34 +1000

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -6,19 +6,10 @@ function Task(props) {
 
 function List(props) {
 
-	//// Old For Loop
-	// let tasks = [];
-	// for (let i = 0; i < props.tasks.length; i++) {
-	// 	let description = props.tasks[i].description;
-	// 	let completed = props.tasks[i].completed;
-	// 	tasks.push(<Task description={description} completed={completed} />);
-	// }
-
 	return (
 		<div>
 			<h1>{ props.heading }</h1>
 			<ul>
-				{/* { tasks } */}
 				{ props.tasks.map(task => <Task description={task.description} completed={task.completed} />) }
 			</ul>
 		</div>
